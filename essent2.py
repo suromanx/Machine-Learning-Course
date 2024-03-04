13a8628ad26e213e09684687c2b4845eea629ac6
romanasbananas <societyorenb@mail.ru>
2024-03-04 14:07:02 +0500

    it is needed to sort out how it works

---
+++ essent2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+A = np.array([[2, 6, 5, 2],
+              [2, -2, 2, 3],
+              [1, 5, 4, 0]
+              ])
+B = np.array([[2, 9, 0, 3],
+              [3, 6, 8, -2],
+              [1, 3, 5, 0],
+             ])
+print(A.shape)
+print(B.shape)
+print()
