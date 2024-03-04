6d0bb77728a197c34b653535445a36f1d1f0c7e4
Roman Sukhoruchkin <societyorenb@mail.ru>
2024-03-04 12:31:01 +0500

    Add files via upload

--- find_substr.py
+++ find_substr.py
@@ -1,12 +1,5 @@
-def find_substr(letters, str):
-    the_result = []
-    for i in range(len(str) - len(letters) + 1):
-        candidate = str[i:i + len(letters)]
-        if candidate == letters:
-            the_result.append(candidate)
-
-    return the_result
-
-
-letters = ['мы']
-str = ['Летом мы хотим отдыхать на море']
+def find_substr(substring,string):
+    for i in range(len(string) - len(substring) + 1):
+        candidate = string[i:i + len(substring)]
+        if candidate == substring:
+            return ((i, i + len(substring)))
